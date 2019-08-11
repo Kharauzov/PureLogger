3c4816fd562a707444509cb55f7c678c3625f7c0
Serhii Kharauzov <serhii.kharauzov@gmail.com>
2019-08-11 18:24:51 +0300

    Add support of macOS and tvOS platforms

---
+++ PureLogger-tvOS/PureLogger_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  PureLogger_tvOS.h
+//  PureLogger-tvOS
+//
+//  Created by Serhii Kharauzov on 8/11/19.
+//  Copyright © 2019 Lollipop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PureLogger_tvOS.
+FOUNDATION_EXPORT double PureLogger_tvOSVersionNumber;
+
+//! Project version string for PureLogger_tvOS.
+FOUNDATION_EXPORT const unsigned char PureLogger_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PureLogger_tvOS/PublicHeader.h>
+
+
